327a1be890430ae2d974cea5d2ccc417ae3aeda1
prince-a-byte <roy1234prin@gmail.com>
2025-12-17 18:30:50 +0530

    Completed Perceptron Trick

--- MyML/src/linear_model.py
+++ MyML/src/linear_model.py
@@ -1,6 +1,13 @@
 # Making the Linear Regression Model.......
 
 import numpy as np
+from itertools import combinations_with_replacement
+from sklearn.preprocessing import PolynomialFeatures
+
+
+# Some functions that are used in this code
+def step(z):
+    return 1 if z>0 else 0
 
 class LinearRegression:
 
@@ -63,3 +70,54 @@ class LinearRegression:
     def predict(self,X_test):
 
         return np.dot(X_test,self.coef_) + self.intercept_
+
+
+class PolynomialRegression:
+    pass
+
+class Perceptron:
+    
+    def __init__(self,learning_rate=0.1,epochs=1000):
+        self.epochs = epochs
+        self.lr = learning_rate
+        self.weights = None
+
+
+    def fit(self,X_train,y_train):
+
+        X = np.insert(X_train,0,1,axis=1)
+
+        # Initializing weights.
+        self.weights = np.ones(X.shape[1])
+
+
+        for i in range(self.epochs):
+
+            # Selecting a random data point
+            idx = np.random.randint(1,X.shape[0])
+
+            # Calculating the Predicted value.
+            y_hat = 1 if np.dot(X[idx], self.weights) > 0 else 0
+
+            # Updating the weights
+            self.weights = self.weights + self.lr * (y_train[idx] - y_hat) * X[idx]
+
+        self.coef_ = self.weights[1:]
+        self.intercept_ = self.weights[0]
+
+  
+        return self.coef_,self.intercept_
+
+    def predict(self,X_test):
+        X = np.insert(X_test,0,1,axis=1)
+        y_pred = np.array([])
+        for i in range(X.shape[0]):
+          if np.dot(X[i],self.weights)>0 :
+            y_pred = np.append(y_pred,1)
+          else:
+            y_pred = np.append(y_pred,0)
+
+        return y_pred
+
+class LogisticRegression:
+    pass
